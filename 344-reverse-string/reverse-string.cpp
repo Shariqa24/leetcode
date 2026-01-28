20aebad2a4df895ab5dff78145847c3b32f0049d
Shariqa Rasheed <shariqarasheed77@gmail.com>
2026-01-28 20:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (51.00%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& str) {
-        int st=0,end=str.size()-1;
-        while(st<end){
-          swap(str[st],str[end]);
-            st++;
-            end--;
-        }
+        reverse(str.begin(),str.end());
+
     }
 };
